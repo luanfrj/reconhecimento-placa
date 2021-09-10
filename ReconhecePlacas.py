3482494598fd3a7d05ccf668e33bba42ab2c9b42
Luan Ferreira Reis de Jesus <luan@luan.heliohost.org>
2021-09-09 22:26:00 -0300

    Melhorias no reconhecimento de placas

--- ReconhecePlacas.py
+++ ReconhecePlacas.py
@@ -4,90 +4,102 @@ import cv2 as cv
 import numpy as np
 import imutils
 import pytesseract
-#import easyocr
+import os
 
-print(os.listdir())
+def obtem_lista_imagens():
+  return os.listdir('images/')
 
-img = cv.imread('images/cropped_parking_lot_10.JPG')
-gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+def detecta_reconhece_placa(img_file_name):
+  img = cv.imread(img_file_name)
+  gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
-# Converte a imagem para tons de cinza
-img_blur = cv.GaussianBlur(gray, (5, 5), 0)
+  # Converte a imagem para tons de cinza
+  img_blur = cv.GaussianBlur(gray, (5, 5), 0)
 
-# edged = img_blur.astype(np.float32) - gray.astype(np.float32)
-# edged = np.where(edged < 2, 0, edged)
-# edged = np.absolute(edged).astype(np.uint8)
-# edged = cv.equalizeHist(edged)
-# _, edged = cv.threshold(edged, 127, 255, cv.THRESH_BINARY)
+  # edged = img_blur.astype(np.float32) - gray.astype(np.float32)
+  # edged = np.where(edged < 2, 0, edged)
+  # edged = np.absolute(edged).astype(np.uint8)
+  # edged = cv.equalizeHist(edged)
+  # _, edged = cv.threshold(edged, 127, 255, cv.THRESH_BINARY)
 
-edged = cv.Canny(img_blur, 70, 150) #Edge detection
-struct_elem = np.array([[1, 1, 1], [1, 1, 1], [1, 1, 1]], np.uint8)
+  edged = cv.Canny(img_blur, 70, 150) #Edge detection
+  struct_elem = np.array([[1, 1, 1], [1, 1, 1], [1, 1, 1]], np.uint8)
 
-# Dilata a imagem
-edged = cv.dilate(edged, struct_elem, iterations = 1)
+  # Dilata a imagem
+  edged = cv.dilate(edged, struct_elem, iterations = 1)
 
-#cv.imshow('IMG_EDGES', edged)
+  #cv.imshow('IMG_EDGES', edged)
 
-# encontra os contornos da imagem
-keypoints = cv.findContours(edged.copy(), cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-contours = imutils.grab_contours(keypoints)
-contours = sorted(contours, key=cv.contourArea, reverse=True)[:10]
+  # encontra os contornos da imagem
+  keypoints = cv.findContours(edged.copy(), cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+  contours = imutils.grab_contours(keypoints)
+  contours = sorted(contours, key=cv.contourArea, reverse=True)[:10]
 
-img_copy = img.copy()
-location = None
-i = 0
-# Para cada contorno encontrado na imagem
-for contour in contours:
-  approx = cv.approxPolyDP(contour, 10, True)
-  if len(approx) == 4:
-    location = approx
-  
-    mask = np.zeros(gray.shape, np.uint8)
-    new_image = cv.drawContours(mask, [location], 0,255, -1)
-    new_image = cv.bitwise_and(img, img, mask=mask)
-
-    # i = i + 1
-    # imgName = 'NEW_IMG' + str(i)
-    # cv.imshow(imgName, new_image)
-
-    (x,y) = np.where(mask==255)
-    (x1, y1) = (np.min(x), np.min(y))
-    (x2, y2) = (np.max(x), np.max(y))
-    cropped_image = gray[x1:x2+1, y1:y2+1]
-
-    i = i + 1
-    imgName = 'IMG_CROP' + str(i)
-    cv.imshow(imgName, cropped_image)
-
-    # Faz a limiarização da imagem para convertê-la em uma imagem binária
-    _, cropped_image = cv.threshold(cropped_image, 50, 255, cv.THRESH_BINARY)
-
-    struct_elem = np.array([[1, 1, 1], [1, 1, 1], [1, 1, 1]], np.uint8)
-    cropped_image = cv.erode(cropped_image, struct_elem, iterations = 3)
-    # cropped_image = cv.morphologyEx(cropped_image, cv.MORPH_CLOSE, struct_elem)
-    # cropped_image = cv.morphologyEx(cropped_image, cv.MORPH_CLOSE, struct_elem)
-    # cropped_image = cv.morphologyEx(cropped_image, cv.MORPH_CLOSE, struct_elem)
-
-    imgName = 'IMG_BIN' + str(i)
-    cv.imshow(imgName, cropped_image)
-
-    # Configura o Pytesseract
-    config = r'-c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 --psm 7'
+  img_copy = img.copy()
+  location = None
+  i = 0
+  # Para cada contorno encontrado na imagem
+  text_result = ""
+  for contour in contours:
+    approx = cv.approxPolyDP(contour, 10, True)
+    if len(approx) == 4:
+      location = approx
     
-    result = pytesseract.image_to_string(cropped_image, lang='eng', config=config)
-    result = result.replace("\n","").replace("\f","")
+      mask = np.zeros(gray.shape, np.uint8)
+      new_image = cv.drawContours(mask, [location], 0,255, -1)
+      new_image = cv.bitwise_and(img, img, mask=mask)
 
-    if len(result) >= 2:
-      text = result
-      font = cv.FONT_HERSHEY_SIMPLEX
-      res = cv.putText(img_copy, text=text, org=(approx[0][0][0], approx[1][0][1]+60), fontFace=font, fontScale=1, color=(0,255,0), thickness=2, lineType=cv.LINE_AA)
-      res = cv.rectangle(img_copy, tuple(approx[0][0]), tuple(approx[2][0]), (0,255,0),3)
-    
-cv.imshow('Result', img_copy)
+      # i = i + 1
+      # imgName = 'NEW_IMG' + str(i)
+      # cv.imshow(imgName, new_image)
 
-if location is None:
-  detected = 0
-  print ("No contour detected")
+      (x,y) = np.where(mask==255)
+      (x1, y1) = (np.min(x), np.min(y))
+      (x2, y2) = (np.max(x), np.max(y))
+      cropped_image = gray[x1:x2+1, y1:y2+1]
 
-cv.waitKey(0)
-cv.destroyAllWindows()
+      i = i + 1
+      imgName = 'IMG_CROP' + str(i)
+      # cv.imshow(imgName, cropped_image)
+
+      # Faz a limiarização da imagem para convertê-la em uma imagem binária
+      _, cropped_image = cv.threshold(cropped_image, 50, 255, cv.THRESH_BINARY)
+
+      struct_elem = np.array([[1, 1, 1], [1, 1, 1], [1, 1, 1]], np.uint8)
+      cropped_image = cv.erode(cropped_image, struct_elem, iterations = 3)
+      # cropped_image = cv.morphologyEx(cropped_image, cv.MORPH_CLOSE, struct_elem)
+      # cropped_image = cv.morphologyEx(cropped_image, cv.MORPH_CLOSE, struct_elem)
+      # cropped_image = cv.morphologyEx(cropped_image, cv.MORPH_CLOSE, struct_elem)
+
+      imgName = 'IMG_BIN' + str(i)
+      # cv.imshow(imgName, cropped_image)
+
+      # Configura o Pytesseract
+      config = r'-c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 --psm 7'
+      
+      result = pytesseract.image_to_string(cropped_image, lang='eng', config=config)
+      result = result.replace("\n","").replace("\f","")
+
+      if len(result) >= 2:
+        text_result = result
+        text = result
+        font = cv.FONT_HERSHEY_SIMPLEX
+        res = cv.putText(img_copy, text=text, org=(approx[0][0][0], approx[1][0][1]+60), fontFace=font, fontScale=1, color=(0,255,0), thickness=2, lineType=cv.LINE_AA)
+        res = cv.rectangle(img_copy, tuple(approx[0][0]), tuple(approx[2][0]), (0,255,0),3)
+      
+  cv.imshow('Result', img_copy)
+
+  if location is None:
+    detected = 0
+    print ("No contour detected")
+  cv.destroyAllWindows()
+
+  return text_result
+
+if __name__ == "__main__":
+  lista_imagens = obtem_lista_imagens()
+  lista_result = []
+  for image_file_name in lista_imagens:
+    print(image_file_name)
+    lista_result.append(detecta_reconhece_placa("images/" + image_file_name))
+  print(lista_result)
