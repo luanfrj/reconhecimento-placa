7cb2c108831dc3fdb13b1d07c8862c6315ae9061
Luan Ferreira Reis de Jesus <luan@luan.heliohost.org>
2021-09-13 11:25:59 -0300

    Modificação no código

--- ReconhecePlacas.py
+++ ReconhecePlacas.py
@@ -6,29 +6,27 @@ import imutils
 import pytesseract
 import os
 
+debug = True
+
 def obtem_lista_imagens():
   return os.listdir('images/')
 
 def detecta_reconhece_placa(img_file_name):
   img = cv.imread(img_file_name)
-  gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
   # Converte a imagem para tons de cinza
+  gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
   img_blur = cv.GaussianBlur(gray, (5, 5), 0)
 
-  # edged = img_blur.astype(np.float32) - gray.astype(np.float32)
-  # edged = np.where(edged < 2, 0, edged)
-  # edged = np.absolute(edged).astype(np.uint8)
-  # edged = cv.equalizeHist(edged)
-  # _, edged = cv.threshold(edged, 127, 255, cv.THRESH_BINARY)
-
   edged = cv.Canny(img_blur, 70, 150) #Edge detection
-  struct_elem = np.array([[1, 1, 1], [1, 1, 1], [1, 1, 1]], np.uint8)
 
   # Dilata a imagem
+  struct_elem = np.array([[1, 1, 1], [1, 1, 1], [1, 1, 1]], np.uint8)
   edged = cv.dilate(edged, struct_elem, iterations = 1)
 
-  #cv.imshow('IMG_EDGES', edged)
+  if (debug):
+    cv.imshow('IMG_EDGES', edged)
 
   # encontra os contornos da imagem
   keypoints = cv.findContours(edged.copy(), cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
@@ -41,7 +39,8 @@ def detecta_reconhece_placa(img_file_name):
   # Para cada contorno encontrado na imagem
   text_result = ""
   for contour in contours:
-    approx = cv.approxPolyDP(contour, 10, True)
+    peri = cv.arcLength(contour, True)
+    approx = cv.approxPolyDP(contour, 0.018 * peri, True)
     if len(approx) == 4:
       location = approx
     
@@ -49,30 +48,33 @@ def detecta_reconhece_placa(img_file_name):
       new_image = cv.drawContours(mask, [location], 0,255, -1)
       new_image = cv.bitwise_and(img, img, mask=mask)
 
-      # i = i + 1
-      # imgName = 'NEW_IMG' + str(i)
-      # cv.imshow(imgName, new_image)
+      i = i + 1
+      if(debug):
+        imgName = 'NEW_IMG' + str(i)
+        cv.imshow(imgName, new_image)
 
       (x,y) = np.where(mask==255)
       (x1, y1) = (np.min(x), np.min(y))
       (x2, y2) = (np.max(x), np.max(y))
       cropped_image = gray[x1:x2+1, y1:y2+1]
 
-      i = i + 1
-      imgName = 'IMG_CROP' + str(i)
-      # cv.imshow(imgName, cropped_image)
+      if(debug):
+        imgName = 'IMG_CROP' + str(i)
+        cv.imshow(imgName, cropped_image)
+
+      # escala a imagem
+      cropped_image = cv.resize(cropped_image, None, fx=2, fy=2, interpolation=cv.INTER_CUBIC)
 
       # Faz a limiarização da imagem para convertê-la em uma imagem binária
       _, cropped_image = cv.threshold(cropped_image, 50, 255, cv.THRESH_BINARY)
 
+      # erosão para aprimorar as letras (pretas)
       struct_elem = np.array([[1, 1, 1], [1, 1, 1], [1, 1, 1]], np.uint8)
       cropped_image = cv.erode(cropped_image, struct_elem, iterations = 3)
-      # cropped_image = cv.morphologyEx(cropped_image, cv.MORPH_CLOSE, struct_elem)
-      # cropped_image = cv.morphologyEx(cropped_image, cv.MORPH_CLOSE, struct_elem)
-      # cropped_image = cv.morphologyEx(cropped_image, cv.MORPH_CLOSE, struct_elem)
 
-      imgName = 'IMG_BIN' + str(i)
-      # cv.imshow(imgName, cropped_image)
+      if(debug):
+        imgName = 'IMG_BIN' + str(i)
+        cv.imshow(imgName, cropped_image)
 
       # Configura o Pytesseract
       config = r'-c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 --psm 7'
@@ -89,10 +91,12 @@ def detecta_reconhece_placa(img_file_name):
       
   cv.imshow('Result', img_copy)
 
+  cv.waitKey(0)
+  cv.destroyAllWindows()
+
   if location is None:
     detected = 0
     print ("No contour detected")
-  cv.destroyAllWindows()
 
   return text_result
 
